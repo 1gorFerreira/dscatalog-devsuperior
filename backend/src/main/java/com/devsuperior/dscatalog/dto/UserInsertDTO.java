3d4f7ca3b90d669d2a0291072b5edbd9f0c00942
Igor <igorferreiradaniel99@hotmail.com>
2022-05-05 20:41:45 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dscatalog.dto;
 
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid // Essa anottation que vai processar por debaixo dos panos a verificação se o email que inserimos já existe no banco;
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dscatalog.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.resources.exceptions.FieldMessage;
+
+//													(Tipo da annotation / Tipo da classe que recebe esse annotation);
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void initialize(UserInsertValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		// Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
+		
+		User user = userRepository.findByEmail(dto.getEmail());
+		
+		if (user != null) {
+			list.add(new FieldMessage("email", "Email já existe!"));
+		}
+		
+		//Está inserindo os erros na lista de errors do beans validation // Inverso do que é feito no ResourceExceptionHandler validation;
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
